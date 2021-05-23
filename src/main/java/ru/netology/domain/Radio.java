265d600721eae7570ba2cc89957b1eecbf30a831
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-05-23 10:31:21 +0300

    modified constructor

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,22 +1,27 @@
 package ru.netology.domain;
 
 public class Radio {
-    private int currentRadioStation;
-    private int minRadioStation;
-    private int maxRadioStation = 10;
-    private int volume;
-    private int minVolume;
+    private int currentRadioStation = 1;
+    private int minRadioStation = 0;
+    private int maxRadioStation;
+    private int volume = 100;
+    private int minVolume = 0;
     private int maxVolume = 100;
 
-    public Radio(int currentRadioStation, int maxRadioStation, int volume, int maxVolume) {
-        this.currentRadioStation = currentRadioStation;
-        this.maxRadioStation = maxRadioStation;
-        this.volume = volume;
-        this.maxVolume = maxVolume;
+    /**
+     * Default constructor.
+     */
+    public Radio() {
+        this.maxRadioStation = 10;
     }
 
+    public Radio(int maxRadioStation) {
+        this.maxRadioStation = maxRadioStation;
+    }
 
-
+    public int getMaxRadioStation() {
+        return this.maxRadioStation;
+    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
